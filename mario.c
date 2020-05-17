9ed24e4f7d4c86ba261c821e0f60cba869e7ab9a
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-17 03:35:33 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -18,18 +18,18 @@ int main(void)
     for (i = 1; i <= n; i++)
     {
        for (h = i; h < n; h++) //It manages the no. of rows in both of the pyramids.
-       {                     
-           printf (" ");       //In this for loop, blank spaces are printed horizontally.
-       }
+        {                     
+           printf(" ");       //In this for loop, blank spaces are printed horizontally.
+        }
        for (w = 1; w <= i; w++)//It manages the no. of columns in the 1st pyramid(i.e. it prints the #s).
-       {                       
-           printf ("#");
-       }
-       printf ("  ");          //It provides space b/w pyramid 1 &2.
+        {                       
+           printf("#");
+        }
+        printf("  ");          //It provides space b/w pyramid 1 &2.
        for (j = 1; j < w; j++) //This loop manages the no. of columns in the 2nd pyramid.
-       {                      
-           printf ("#");       //in every row of pyramid 2, it prints the exact same no. of # as there are in corresponding pyramid 1's row.
-       }
-       printf ("\n");          //It moves the program to the next line.
+        {                      
+           printf("#");       //in every row of pyramid 2, it prints the exact same no. of # as there are in corresponding pyramid 1's row.
+        }
+       printf("\n");          //It moves the program to the next line.
     }
 }
