bfb22e20652e724648d5e5a3709b94b319f6f0da
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-17 04:11:23 +0000

    automated commit by submit50

--- mario.c
+++ mario.c
@@ -5,23 +5,25 @@
 int main(void)
 {
   int n, height, width, i;
+  //n is the value entered by the user.
+  //i is initial variable needed to start the pattern by providing a starting value.
   do
   {
      n = get_int("Enter any number between 0 & 8: ");
   }
-  while(n > 8 || n <= 0);
-  for(i=1;i<=n;i++)
-  {
-    for(height=i;height<n;height++)
-    {
-      printf(" ");
-    }
-    for(width=1;width<=i;width++)
-    {
-      printf("#");
-    }
-    printf("\n");
-  }
+  while (n > 8 || n <= 0);//This is to limit the acceptable value to be b/w 0 & 8.
+  for (i = 1; i <= n; i++)
+   {
+        for (height = i; height < n; height++)
+        {
+           printf(" ");
+        }
+        for (width = 1; width <= i; width++)
+        {
+           printf("#");
+        }
+        printf("\n");
+   }
   
 }  
     
