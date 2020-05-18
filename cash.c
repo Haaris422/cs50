4188964fc1cc7156f5717bbd50a51d700bdebaba
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-18 00:19:31 +0000

    automated commit by check50 [check50=True]

--- cash.c
+++ cash.c
@@ -14,13 +14,13 @@ int main(void)
     int cents = round(change*100);
     int coins = 0;
     
-    int denominations[] = {25, 10, 5, 1};
+    int c[] = {25, 10, 5, 1};
     int size = 4;
     
     for(int i = 0; i < size; i++)
     {
-        coins += cents/denominations[i];
-        cents %= denominations[i];
+        coins += cents/c[i];
+        cents %= c[i];
     }
     printf("Least no. of coins to be dispensed -> %d\n", coins);
 }
