0c8ccdc6658a248dd6d77813325e5ee495840865
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-19 05:05:34 +0000

    automated commit by submit50

--- readability.c
+++ readability.c
@@ -7,23 +7,23 @@
 int main(void)
 {
     float letters = 0, words = 0, sentences = 0;
-    string Text = get_string("Text: ");
-    int i, n = strlen(Text);
-    if (isalnum(Text[0]))
+    string Text = get_string("Text: "); //This records the user input.
+    int i, n = strlen(Text); //This is to create an array from the line of text entered by the user.
+    if (isalnum(Text[0])) //This is to set an initial value of 1 word for any line of text we want to evaluate.
     {
         words = 1;
     }
     for (i = 0; i < n; i++)
     {
-        if (isalpha(Text[i]))
+        if (isalpha(Text[i])) //To calculate the no. of letters.
         {
             letters++;
         }
-        if (isspace(Text[i]))
+        if (isspace(Text[i])) //To calculate the no. of words.
         {
             words++;
         }
-        if (Text[i] == '!' || Text[i] == '?' || Text[i] == '.')
+        if (Text[i] == '!' || Text[i] == '?' || Text[i] == '.') //To calculate the no. of sentences.
         {
             sentences++;
         }
@@ -31,25 +31,25 @@ int main(void)
     int j = round(letters);
     int k = round(words);
     int l = round(sentences);
+    //i,j & k are used to convert float to int.
     printf("Letters present: %d\n", j);
     printf("Words present: %d\n", k);
     printf("Sentences present: %d\n", l);
-    float L = (letters*100)/words;
-    float S = (sentences*100)/words;
-    printf("%f\n", L);
-    printf("%f\n", S);
-    float index = 0.0588*L - 0.296*S -15.8;
-    int I = round(index);
-    printf("%d\n", I);
-    if (I < 1)
+    float L = (letters * 100) / words; //Letters per 100 words.
+    float S = (sentences * 100) / words; //Sentences per 100 words.
+    printf("L = %f\n", L);
+    printf("S = %f\n", S);
+    float index = 0.0588 * L - 0.296 * S - 15.8; //Coleman-Liau formula. 
+    int I = round(index); //Used to convert float into int.
+    if (I < 1) //Used to print grade if I<1.
     {
         printf("Before Grade 1\n");
     }
-    else 
+    else //Used to print grade if I is b/w 1 & 16.
     {
         printf("Grade %d\n", I);
     }
-    if (I >= 16)
+    if (I >= 16) //Used to print grade if I is >= 16
     {
         printf("Grade 16+\n");
     }
