7890c5e8dd29600cac06f20465a566116039f2fb
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-23 01:15:16 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -8,6 +8,11 @@ int main (int argc, string argv[])
 {
    if ( argc == 2 && isdigit (*argv[1]))
    {
+       if (isalnum (*argv[1]))
+       {
+           printf("Useage: ./caesar key\n");
+           return 1;
+       }
        int k = atoi(argv[1]);   
        string s = get_string("plaintext: ");
        int n = strlen(s);
