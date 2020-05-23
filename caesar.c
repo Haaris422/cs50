c0e93df05ec39459d5b46247b2e690c4dfa2b9d4
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-23 00:47:47 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -6,7 +6,7 @@
 
 int main (int argc, string argv[])
 {
-   if ( argc == 2 && isdigit(*argv[1]))
+   if ( argc == 2 && isdigit (*argv[1]))
    {
        int k = atoi(argv[1]);   
        string s = get_string("plaintext: ");
