1577e934263083dad03cadad1c1c17f402c6a476
Haaris422 <Haaris422@users.noreply.github.com>
2020-05-23 01:55:42 +0000

    automated commit by check50 [check50=True]

--- caesar.c
+++ caesar.c
@@ -3,45 +3,54 @@
 #include <string.h>
 #include <stdlib.h>
 #include <ctype.h>
+bool check_key(string a);
 
 int main (int argc, string argv[])
 {
-   if ( argc == 2 && isdigit (*argv[1]))
-   {
-       if (isalpha (*argv[1]))
+  if (argc != 2 || !check_key(argv[1]))
+  {
+      printf ("Usage: ./caesar key\n");
+      return 1;
+  }
+  else
+  {
+  int k = atoi(argv[1]);
+  string s = get_string("plaintext: ");
+  int n = strlen(s);
+  printf("ciphertext: ");
+  for (int i = 0; i <n; i++)
+  {
+       if (isalpha(s[i]))
        {
-           printf("Useage: ./caesar key\n");
-           return 1;
-       }
-       int k = atoi(argv[1]);   
-       string s = get_string("plaintext: ");
-       int n = strlen(s);
-       printf("ciphertext: ");
-       for (int i = 0; i <n; i++)
+           if (isupper(s[i]))
+           {
+                printf("%c", (((s[i] - 'A') + k) %26) + 'A');
+           }
+           else if (islower(s[i]))
+           {
+                char c = s[i] - 'a';
+                printf ("%c", (c + k)%26 + 'a');
+           }
+       }   
+       else
        {
-           if (isalpha(s[i]))
-           {
-               if (isupper(s[i]))
-               {
-                   printf("%c", (((s[i] - 'A') + k) %26) + 'A');
-               }
-               else if (islower(s[i]))
-               {
-                   char c = s[i] - 'a';
-                   printf ("%c", (c + k)%26 + 'a');
-               }
-           }
-           else
-           {
-               printf ("%c", s[i]);
-           }
+            printf ("%c", s[i]);
        }
-       printf("\n");
-       return 0;
-   }
-   else 
-   {
-       printf ("Usage: ./caesar key\n");
-       return 1;
-   }
+  }
+  printf("\n");
+  return 0;
+  }
+}
+
+
+bool check_key(string a)
+{
+    for (int i = 0, n = strlen(a); i < n; i++)
+    
+        if(!isdigit(a[i]))
+        
+            return false;
+        
+        return true;
+    
 }
