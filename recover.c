5136568bb21dd6894e4e30a237fe2ac566b3c70b
Haaris422 <Haaris422@users.noreply.github.com>
2020-07-24 10:50:18 +0000

    automated commit by submit50

--- recover.c
+++ recover.c
@@ -3,24 +3,24 @@
 #include <stdint.h>
 #include <cs50.h>
 
-#define B_S 512
-#define F_N_S 8
+#define B_S 512 //Block Size set to 512 bytes.
+#define F_N_S 8 //File Size set to 8 bytes.
 
-typedef uint8_t BYTE;
+typedef uint8_t BYTE; //To create BYTE.
 
-bool start_new_jpeg(BYTE buffer[]);
+bool start_new_jpeg(BYTE buffer[]); //To check if a file is jpeg or not.
 
 int main(int argc, char *argv[])
 {
-    if (argc != 2)
+    if (argc != 2) //To make sure only one argument is entered.
     {
         printf("Error: ./recover enter 1 argument only.\n");
         return 1;
     }
-    FILE *If = fopen(argv[1], "r");
+    FILE *If = fopen(argv[1], "r"); //To open the file entered as the argument.
     if (If == NULL)
     {
-        printf("Error: File not found");
+        printf("Error: File not found"); //In case the name entered is not an existing file.
         return 1;
     }
 
@@ -28,13 +28,13 @@ int main(int argc, char *argv[])
     int file_index = 0;
     bool first_found = false;
     FILE *Of;
-    while (fread (buffer, B_S, 1, If))
+    while (fread(buffer, B_S, 1, If)) //Used to read and identify the file as a jpeg or not.
     {
         if (start_new_jpeg(buffer))
         {
             if (!first_found)
             {
-                first_found = true;
+                first_found = true; //Turns it to to true.
             }
             else
             {
@@ -42,7 +42,7 @@ int main(int argc, char *argv[])
             }
             char filename[F_N_S];
             sprintf(filename, "%03i.jpg", file_index++);
-            Of = fopen(filename, "w");
+            Of = fopen(filename, "w"); //To write the file.
             if (Of == NULL)
             {
                 return 1;
@@ -57,7 +57,11 @@ int main(int argc, char *argv[])
     fclose(Of);
     fclose(If);
 }
+
+
 bool start_new_jpeg(BYTE buffer[])
 {
-    return buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0;
+    return buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0; 
+    //Because the first 4 digits of any jpeg are 0xff, 0xd8, 0xff & 0xe(1 to f).
+    //To easily and quickly identify the 4th digit, the 0xe part is kept while the last no. is converted to 0.
 }
