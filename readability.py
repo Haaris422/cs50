9da1bad3580fd2557bd40910e21a7fc678e2967f
Haaris422 <Haaris422@users.noreply.github.com>
2020-09-03 15:29:57 +0000

    automated commit by submit50

--- readability.py
+++ readability.py
@@ -1,22 +1,22 @@
 from cs50 import get_string
 
-text = get_string ("Text: ").strip()
-words = 0
-letters = 0
-sentences = 0
+text = get_string("Text: ").strip()  # Asks for the usrers input.
+words = 0  # number of words.
+letters = 0  # Nnumber of letters.
+sentences = 0  # Number of sentences.
 for i in range(len(text)):
-    words = len(text.split())
-    if text[i].isalpha() == True:
+    words = len(text.split())  # Counts the no. of words in the text.
+    if text[i].isalpha() == True:  # Countss the no. of letters in the text.
         letters += 1
-    elif text[i] in ['?', '.', '!']:
+    elif text[i] in ['?', '.', '!']:  # Countss the no. of sentences in the text.
         sentences += 1
 L = (letters/words * 100)
-S = (sentences/words *100)
-I = round(0.0588 * L - 0.296 * S - 15.8)
+S = (sentences/words * 100)
+I = round(0.0588 * L - 0.296 * S - 15.8)  # Coleman-Liau formula
 if I < 1:
     print("Before Grade 1")
 elif I >= 16:
-    print ("Grade 16+")
+    print("Grade 16+")  # I = Index/Grade Level
 else:
-    print (f"Grade {I}")
+    print(f"Grade {I}")
     
