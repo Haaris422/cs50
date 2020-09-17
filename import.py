5f9752aefc5b88ab2622cb5e3ff365aeff14e0f4
Haaris422 <Haaris422@users.noreply.github.com>
2020-09-17 00:06:07 +0000

    automated commit by check50 [check50=True]

--- import.py
+++ import.py
@@ -1,25 +1,18 @@
+import cs50
 from sys import argv, exit
 import csv
-import cs50
 
-def F_N(full_name):
-    names = row["name"].split()
-    return names if len(names) >= 3 else [names[0], None, names[1]] 
-    
+def dividedN(N):
+    names = N.split()
+    return names if len(names) >= 3 else [names[0], None, names[1]]
+
 if len(argv) != 2:
-    print("Usaage error: only input 1 argument")
+    print("Usage Error: incorrect number of arguments")
     exit(1)
-
 db = cs50.SQL("sqlite:///students.db")
-
-with open(argv[1], 'r') as csvf:
-    D = csv.DictReader(csvf, delimiter = ",")
-    
-
+with open (argv[1], 'r') as csvf:
+    D = csv.DictReader(csvf)
     for row in D:
-       
-        names = F_N(row["name"])
-        db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?, ?, ?, ?, ?)", names[0], names[1], names[2], row["house"], row["birth"])
-        house = row["birth"]
-        birth = row["birth"]
-        
+        names = dividedN(row["name"])
+        db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)", names[0], names[1], names[2], row["house"], row["birth"])
+     
--- roster.py
+++ roster.py
@@ -1,12 +1,12 @@
 from sys import argv, exit
-import csv
 import cs50
 
 if len(argv) != 2:
-    print("Usaage error: only input 1 argument")
+    print("Usage Error: incorrect number of arguments")
     exit(1)
-db = cs50.SQL("sqlite:///students.db") 
-table = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", (argv[1]))
-for row in table:
+db = cs50.SQL("sqlite:///students.db")
+rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[1])
+for row in rows:
     first, middle, last, birth = row["first"], row["middle"], row["last"], row["birth"]
-    print(f"{first} {middle + ' ' if middle else ''} {last} born {birth}")
+    print(f"{first} {middle + ' ' if middle else ''} {last}, born {birth}")
+    
