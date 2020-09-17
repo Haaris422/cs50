28ee3089073994cc4b95277009add0d4a8163fbc
Haaris422 <Haaris422@users.noreply.github.com>
2020-09-17 00:33:23 +0000

    automated commit by submit50

--- import.py
+++ import.py
@@ -2,17 +2,17 @@ import cs50
 from sys import argv, exit
 import csv
 
-def dividedN(N):
+def dividedN(N): # To split names.
     names = N.split()
     return names if len(names) >= 3 else [names[0], None, names[1]]
 
-if len(argv) != 2:
+if len(argv) != 2: # Shows error if incorrect no. of arguments has entered.
     print("Usage Error: incorrect number of arguments")
     exit(1)
-db = cs50.SQL("sqlite:///students.db")
+db = cs50.SQL("sqlite:///students.db") # For reading the db file.
 with open (argv[1], 'r') as csvf:
-    D = csv.DictReader(csvf)
+    D = csv.DictReader(csvf) # Makes a dictionary out of every row. 
     for row in D:
         names = dividedN(row["name"])
         db.execute("INSERT INTO students(first, middle, last, house, birth) VALUES(?,?,?,?,?)", names[0], names[1], names[2], row["house"], row["birth"])
-     
+        # Inserts the divided anes into the name divisions.
--- roster.py
+++ roster.py
@@ -1,12 +1,11 @@
 from sys import argv, exit
 import cs50
 
-if len(argv) != 2:
+if len(argv) != 2: # To stop if wrong no. of inputs have been entered.
     print("Usage Error: incorrect number of arguments")
     exit(1)
 db = cs50.SQL("sqlite:///students.db")
-rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[1])
+rows = db.execute("SELECT * FROM students WHERE house = ? ORDER BY last, first", argv[1]) # To find the students of the entered house.
 for row in rows:
     first, middle, last, birth = row["first"], row["middle"], row["last"], row["birth"]
-    print(f"{first} {middle +' ' if middle else ''}{last}, born {birth}")
-    
+    print(f"{first} {middle +' ' if middle else ''}{last}, born {birth}") # To print the students of the entered house.
